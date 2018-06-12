563d2d5112f6391ee291207ba7597936929e1a3e
IsaacCarrada <isaaccarrada@gmail.com>
2018-06-11 21:30:43 -0500

    Primer division en componentes de carrito

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,14 @@ import { Route, Switch } from 'react-router-dom'
 import App from './components/App'; //lo primero que hace es buscar un index.js, si no se debe declarar
 import Home from './components/Home';
 import Page404 from './components/Page404';
+import checkout from './components/Checkout';
 
 const AppRoutes = () =>
 <App> 
     {/* aqui abrí app */}
     <Switch> 
         <Route exact path ="/" component={Home}/> {/*Siempre que sea la ruta integra ira a home*/}
+        <Route path="/Checkout" component={checkout}/>
         <Route component={Page404} /> {/*Este sería mi else*/}
     </Switch>
 </App>
