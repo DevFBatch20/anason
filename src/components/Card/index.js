6a6fcf5d6196e11b0bfe00c45d507293a6ffc82f
Mikee Rmz <mikee.rmz@gmail.com>
2018-06-15 03:12:44 -0500

    Add axios and props to cards
    Signed-off-by: Mikee Rmz <mikee.rmz@gmail.com>

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,31 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Card, Button, CardImg, CardTitle, CardText, CardDeck,
  CardSubtitle, CardBody } from 'reactstrap';
+import PropTypes from 'prop-types';
 
-const Example = (props) => {
-  return (
-    <CardDeck>
+class Example extends Component {
+  static propTypes={
+    itemProduct: PropTypes.object.isRequired
+  }
+  render() {
+    const {itemProduct} = this.props
+    console.log(itemProduct)
+    return (
       <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
+        <CardImg top width="100%" src={itemProduct.image} alt={itemProduct.shortName} />
         <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+          <CardTitle>{itemProduct.shortName}</CardTitle>
+          <CardSubtitle>Description</CardSubtitle>
+          <CardText>{itemProduct.description}</CardText>
           <Button>Button</Button>
         </CardBody>
       </Card>
-      <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-
-    </CardDeck>
-  );
+   );
+  }
 };
 
 export default Example;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,40 @@
 //appjs es el body con el que voy a dividir la aplicación, generalmente es header, body y footer
 //Dependencias
 import React, { Component } from 'react';
+import axios from 'axios';
+import { CardDeck, Row, Col } from 'reactstrap';
+// Components
 import Carousel from '../Carousel'
 import Card from '../Card'
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.items;
+  }
+
+  componentWillMount() {
+    axios.get('https://amazonb20.herokuapp.com/api/v1/topten')
+         .then(response => {
+          this.setState({
+            items: response.data
+          })
+         })
+         .catch(error => {
+          console.log(error)
+         })
+  }
   render() { 
-    console.log("Renderizando esta funcion")
+    const items = this.state.items
     return (
         <div>
-        <Carousel />
-        <Card />
+          <Carousel />
+            <Row>
+              <Col md="3">
+                {items && items.map((item,key) => <Card itemProduct={item}/> )}
+              </Col>
+            </Row>
         </div>
     );
   }
